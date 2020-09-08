047b977cdb7e1f91c836ec4bd48f421c73384a73
dimaarion <dimaarion@gmail.com>
2020-09-08 13:53:37 +0300

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
 export default function App() {
   return (
     <BDiv className = "app">
-     dddd
+    <img src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon2.png"/>
+   <Row >
+
+   </Row>
      
     </BDiv>
   );
