a4d32fa8cf48c0250599c468f9cd146f2c463d85
dimaarion <dimaarion@gmail.com>
2020-09-08 13:58:12 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
 export default function App() {
   return (
     <BDiv className = "app">
-    <img src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon2.png"/>
+    <img className = "fon" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon3.png"/>
    <Row >
 
    </Row>
