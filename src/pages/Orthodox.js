77a9c1bd76bd49d30fd94f935f84a5643e41cdef
dimaarion <dimaarion@gmail.com>
2020-09-08 21:39:53 +0300

    1 file added

---
+++ src/pages/Orthodox.js
@@ -0,0 +1,9 @@
+import React from "react";
+ import { Container } from "bootstrap-4-react";
+function Orthodox(){
+    return (
+     <Container>
+     Страница Находится в разработке
+     </Container>
+    )
+}export default Orthodox
