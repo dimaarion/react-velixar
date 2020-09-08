762679b1e7f21637b911c933e0a33671bad024e4
dimaarion <dimaarion@gmail.com>
2020-09-08 14:11:00 +0300

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ export default function App() {
   return (
     <BDiv className = "app">
     <img className = "fon" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon3.png"/>
+    <img className = "logo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
+    <img className = "photo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface4.png"/>
    <Row >
 <Col>
-<img width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
+
 </Col>
 <Col>
-<img width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
+
 </Col>
    </Row>
      
