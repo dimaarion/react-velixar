eacf89c6d188fbf48c3846f37f158ea8f88b3c4a
dimaarion <dimaarion@gmail.com>
2020-09-08 09:26:35 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
 import "./css/bootstrap.css"
+import "./css/app.css"
 import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
 export default function App() {
   return (
-    <BDiv>
+    <BDiv className = "app">
      dddd
     </BDiv>
   );
