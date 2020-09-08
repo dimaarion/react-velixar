18ecff8db41dad885ccf8a43557414840933e7fc
dimaarion <dimaarion@gmail.com>
2020-09-08 23:18:26 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./style.css";
 import "./css/bootstrap.css";
 import "./css/app.css";
 import Home from "./pages/Home";
+import Orthodox from "./pages/Orthodox";
 import baseUrlSite from "./sittens/baseUrlSite.json";
 import { Container, Row, Col, BDiv } from "bootstrap-4-react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -12,6 +13,9 @@ export default function App() {
       <Switch>
         <Route exact path="/">
           <Home/>
+        </Route>
+        <Route path="/orthodox">
+          <Orthodox/>
         </Route>
       </Switch>
     </Router>
