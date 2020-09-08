8b37120726a62073dc5cc9aa8f2a75e7c36430bc
dimaarion <dimaarion@gmail.com>
2020-09-08 14:25:59 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import "./style.css";
 import "./css/bootstrap.css";
 import "./css/app.css";
 import baseUrlSite from "./sittens/baseUrlSite.json";
-import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
+import { Container, Row, Col, BDiv } from "bootstrap-4-react";
 export default function App() {
-  console.log(baseUrlSite)
+  console.log(baseUrlSite);
   return (
-    <BDiv className = "app">
-    <img className = "fon" src = {baseUrlSite.site + "img/fon3.png"}/>
-    <img className = "logo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
-    <img className = "photo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface4.png"/>
-    <img className = "arnament" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface2.png"/>
-   <Row >
-<Col>
-
-</Col>
-<Col>
-
-</Col>
-   </Row>
-     
+    <BDiv className="app">
+      <img className="fon" src={baseUrlSite.site + "img/fon3.png"} />
+      <img
+        className="logo"
+        width="100%"
+        src={baseUrlSite.site + "img/interface.png"}
+      />
+      <img
+        className="photo"
+        width="100%"
+        src={baseUrlSite.site + "img/interface4.png"}
+      />
+      <img
+        className="arnament"
+        width="100%"
+        src={baseUrlSite.site + "img/interface2.png"}
+      />
+      <Row>
+        <Col />
+        <Col />
+      </Row>
     </BDiv>
   );
 }
