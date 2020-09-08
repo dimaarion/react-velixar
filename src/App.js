d39898c024a14705fe9c6e53a2353f2506289ce3
dimaarion <dimaarion@gmail.com>
2020-09-08 14:12:26 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
 export default function App() {
   return (
     <BDiv className = "app">
+    <img className = "arnament" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface2.png"/>
     <img className = "fon" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon3.png"/>
     <img className = "logo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
     <img className = "photo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface4.png"/>
