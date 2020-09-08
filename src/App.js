5074a05ed745525770ee81b58d83c8433514f96f
dimaarion <dimaarion@gmail.com>
2020-09-08 15:36:48 +0300

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./css/app.css";
 import baseUrlSite from "./sittens/baseUrlSite.json";
 import { Container, Row, Col, BDiv } from "bootstrap-4-react";
 export default function App() {
- 
   return (
     <BDiv className="app">
       <img className="fon" src={baseUrlSite.site + "img/fon3.png"} />
@@ -25,8 +24,18 @@ export default function App() {
         src={baseUrlSite.site + "img/interface2.png"}
       />
       <Row>
-        <Col />
-        <Col />
+        <Col pl = "5" style = {{marginTop:"30%"}}>
+          <img width = "100%" src={baseUrlSite.site + "img/fknp.png"} />
+        </Col>
+         <Col style = {{marginTop:"35%"}}>
+          <img width = "100%" src={baseUrlSite.site + "img/fknp.png"} />
+        </Col>
+         <Col style = {{marginTop:"35%"}}>
+          <img width = "100%" src={baseUrlSite.site + "img/fknp.png"} />
+        </Col>
+         <Col pr = "5" style = {{marginTop:"30%"}}>
+          <img width = "100%" src={baseUrlSite.site + "img/fknp.png"} />
+        </Col>
       </Row>
     </BDiv>
   );
