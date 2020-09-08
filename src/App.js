07cb85f5d253054b34e4e1cb85dd77244428f6af
dimaarion <dimaarion@gmail.com>
2020-09-08 14:22:53 +0300

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./style.css";
-import "./css/bootstrap.css"
-import "./css/app.css"
+import "./css/bootstrap.css";
+import "./css/app.css";
+import baseUrlSite from "./sittens/baseUrlSite.json";
 import { Container, Row, Col, BDiv } from 'bootstrap-4-react';
 export default function App() {
+  console.log(baseUrlSite)
   return (
     <BDiv className = "app">
-    <img className = "arnament" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface2.png"/>
-    <img className = "fon" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/fon3.png"/>
+    <img className = "fon" src = {baseUrlSite.site + "img/fon3.png"}/>
     <img className = "logo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface.png"/>
     <img className = "photo" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface4.png"/>
+    <img className = "arnament" width = "100%" src = "https://cdn.jsdelivr.net/gh/dimaarion/react-velixar@master/public/img/interface2.png"/>
    <Row >
 <Col>
 
